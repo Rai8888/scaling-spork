c6895f20599a6bef6254b91592c753376c7e9b91
Janelaxag <hellhole.gc@gmail.com>
2023-06-11 17:57:39 +0600

    basic logic

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function App() {
+    return <div className="App">
+        <h1>Hotel Booking</h1>
+        <button className="btn btn-primary"></button>
+    </div>;
+}
+
+export default App;
