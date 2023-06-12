bcaebf793ae6efb47834de6d0da01b5427d100bb
Janelaxag <hellhole.gc@gmail.com>
2023-06-12 14:08:54 +0600

    Implement registration and login routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import Navbar from './components/Navbar'
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import HomeScreen from "./screens/HomeScreen";
-import Bookingscreen from "./screens/Bookingscreen";
+import BookingScreen from "./screens/BookingScreen";
+import RegisteScreen from './screens/RegisteScreen';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
     return <div className="App">
         <Navbar />
         <Routes>
             <Route path="/booking" element={<HomeScreen />} />
-            <Route path="/booking/:roomid" component={Bookingscreen} />
+            <Route path="/booking/:roomid" element={<BookingScreen />} />
+            <Route path="/register" element={<RegisteScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
         </Routes>
     </div>;
 }
