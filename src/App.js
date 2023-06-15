1fbaad0e834b75062252ac0de38ce00ceed4e052
Janelaxag <hellhole.gc@gmail.com>
2023-06-15 19:30:40 +0600

    c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import './assets/styles/App.css';
 import NavBar from './components/Navbar.jsx';
 import { routes } from './routes/routes';
 
--- src/screens/LandingScreen.jsx
+++ src/screens/LandingScreen.jsx
@@ -1,5 +1,17 @@
+// import check from '../assets/images/check.png';
+import styles from '../assets/styles/LandingScreen.module.css';
+
 const LandingScreen = () => {
-  return <div>LandingScreen</div>;
+  return (
+    <div className='container'>
+      <div className={styles.wrap}>
+        <div className={styles.title}>
+          <h5>Hotel</h5>
+          <h1>BOOKING</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingScreen;
