3efa680ce8277ebf7fe51fb3b8993409172ad423
Janelaxag <hellhole.gc@gmail.com>
2023-06-23 14:12:55 +0600

    landing screen

--- src/screens/LandingScreen.jsx
+++ src/screens/LandingScreen.jsx
@@ -1,17 +1,5 @@
-// import check from '../assets/images/check.png';
-import styles from '../assets/styles/LandingScreen.module.css';
-
 const LandingScreen = () => {
-  return (
-    <div className='container'>
-      <div className={styles.wrap}>
-        <div className={styles.title}>
-          <h5>Hotel</h5>
-          <h1>BOOKING</h1>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>LandingScreen</div>;
 };
 
 export default LandingScreen;
