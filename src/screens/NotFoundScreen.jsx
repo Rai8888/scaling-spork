1aa58149bf3beed7b1a8b3ac625865bf81eb34d1
Janelaxag <hellhole.gc@gmail.com>
2023-06-24 20:05:47 +0600

    change styles

--- src/screens/NotFoundScreen.jsx
+++ src/screens/NotFoundScreen.jsx
@@ -1,21 +1,21 @@
-import { Result, Button } from 'antd';
+import { Button, Result } from 'antd';
 
 const NotFound = () => {
-  const titleStyle = { color: "#fff" };
-  const buttonStyle = { backgroundColor: "#fff", color: "#000" };
+  const titleStyle = { color: '#000' };
+  const buttonStyle = { backgroundColor: '#000', color: '#fff' };
   return (
     <Result
-      status="404"
+      status='404'
       title={<span style={titleStyle}>404</span>}
       subTitle={<span style={titleStyle}>Извините, страница не найдена или вы не имете доступа</span>}
       extra={
-        <Button type="primary" href="/" style={buttonStyle}>
+        <Button type='primary' href='/' style={buttonStyle}>
           На главную
         </Button>
-
       }
     />
   );
 };
 
 export default NotFound;
+
