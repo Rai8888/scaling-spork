9b7937ca330eb74cf000e724a44789a69fbad68e
Janelaxag <hellhole.gc@gmail.com>
2023-06-24 15:45:11 +0600

    error refactor

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 const Error = () => {
   return (
     <div>
-      <div class='alert alert-danger' role='alert'>
+      <div className='alert alert-danger' role='alert'>
         Something went wrong, pleas try later!
       </div>
     </div>
